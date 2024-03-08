ecc5a0e10900e3ac0e3d4a651990932df8233eb3
Nabeel <nabeel@o2geeks.com>
2024-03-08 17:36:49 +0500

    Remove SceneDelegate and change info.plist location

---
+++ EDYOU/EDYOU/Application/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  EDYOU
+//
+//  Created by imac3 on 08/03/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+
+}
+
